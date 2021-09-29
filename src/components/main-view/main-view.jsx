502bb69c63ed117b56ba66a1f58cce6d21d334f5
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-09-28 23:54:16 -0700

    3.4update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,53 +1,97 @@
 import React from "react";
 import axios from "axios";
+
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { RegistrationView } from "../registration-view/registration-view";
 
 export class MainView extends React.Component{
   //to have a default state.
         constructor(){
           super();
           this.state = {
-            movies: [
-              // { _id: 1, Title: 'Silence of the Lambs', Description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.', Director:"Jonathan Demme", ImagePath: "https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"},
-              // { _id: 2, Title: 'Black Widow', Description: 'A film about Natasha Romanoff in her quests between the films Civil War and Infinity War',Director:"Cate Shortland", ImagePath: 'https://th.bing.com/th/id/R.404727dfbe3c7223503ad0d64fe60771?rik=c4QS5FDTlo91Eg&riu=http%3a%2f%2fwww.loucademiadecinema.com.br%2fwp-content%2fuploads%2f2020%2f04%2fblack-widow-5e667a3ea6e0f.jpg&ehk=A1LcBcqXOEpY3RDZ1jkxwt9ku%2fvzkPv5ZorB2F%2b22Hw%3d&risl=&pid=ImgRaw'},
-              // { _id: 3, Title:"Berlin Syndrome",Description:"update A passionate holiday romance leads to an obsessive relationship, when an Australian photojournalist wakes one morning in a Berlin apartment and is unable to leave.", Director:"Cate Shortland",ImagePath:"https://images-na.ssl-images-amazon.com/images/I/91J7hZMz+sL.jpg"}
-            ],
-            selectedMovie:null
+            movies: [],
+            selectedMovie:null,
+            user:null,
+            register:false
           };
         }
 
         componentDidMount(){
-          axios.get('https://edmund-movie-api.herokuapp.com/')
-          .then(response =>{
+          axios.get('https://edmund-movie-api.herokuapp.com/movies')
+          .then((response) => {
             this.setState({
-              movies:response.data
+              movies: response.data,
             });
           })
-          .catch(error => {
+          .catch((error) => {
             console.log(error);
           });
+      }
+
+        /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+        setSelectedMovie(movie) {
+          this.setState({
+            selectedMovie: movie,
+          });
         }
-        setSelectedMovie(newSelectedMovie) {
+
+        onRegistration(register) {
+          this.setState({
+            register,
+          });
+        }
+          /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+          onLoggedIn(user) {
             this.setState({
-              selectedMovie: newSelectedMovie
+              user,
             });
           }
-          render() {
-            const { movies, selectedMovie } = this.state;
 
+            render() {
+              const { movies, selectedMovie, user, register } = this.state;
 
-            if (movies.length === 0) return <div className="main-view" />;
-        
-            return (
-              <div className="main-view">
-                {selectedMovie
-                  ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-                  : movies.map(movie => (
-                    <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-                  ))
-                }
-              </div>
-            );
+              /* If there is not registered user, the RegisterView is rendered. If there is a user registered,
+               the user details are *passed as a prop to the LoginView*/
+              if (!register)
+                return (
+                  <RegistrationView
+                    onRegistration={(register) => this.onRegistration(register)} />);
+          
+              /* If there is no user, the LoginView is rendered. If there is a user logged in,
+               the user details are *passed as a prop to the LoginView*/
+              if (!user)
+                return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+          
+              // Before the movies have been loaded
+              if (movies.length === 0) return <div className="main-view" />;
+          
+              return (
+                <div className="main-view">
+                  {
+                    /*If the state of `selectedMovie` is not null, that selected movie 
+                    will be returned otherwise, all *movies will be returned*/
+                    selectedMovie ? (
+                      <MovieView
+                        movie={selectedMovie}
+                        onBackClick={(newSelectedMovie) => {
+                          this.setSelectedMovie(newSelectedMovie);
+                        }}
+                      />
+                    ) : (
+                      movies.map((movie) => (
+                        <MovieCard
+                          key={movie._id}
+                          movieData={movie}
+                          onMovieClick={(newSelectedMovie) => {
+                            this.setSelectedMovie(newSelectedMovie);
+                          }}
+                        />
+                      ))
+                    )
+                  }
+                </div>
+              );
+            }
           }
-}
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class MovieView extends React.Component{
 
-    componentDidMount(){
-        document.addEventListener('keypress',event=>{
-            console.log(event.key);
-        });
-    }
+    // componentDidMount(){
+    //     document.addEventListener('keypress',event=>{
+    //         console.log(event.key);
+    //     });
+    // }
     render(){
         const {movie , onBackClick } = this.props;
 
@@ -32,3 +33,10 @@ export class MovieView extends React.Component{
         );
     }
 }
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      ImagePath: PropTypes.string.isRequired
+    }).isRequired
+  };
