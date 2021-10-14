6802a8e94d9d9ea47b65cdce44b4dd4612dbcfb2
Edmundwang8695 <82694353+Edmundwang8695@users.noreply.github.com>
2021-10-13 17:53:24 -0700

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -47,7 +47,7 @@ removeFavoriteMovie(){
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
 
-    axios.delete('https://edmund-movie-api.herokuapp.com/users/:username/movies/:movieId', {
+    axios.delete(`https://edmund-movie-api.herokuapp.com/users/${username}/movies/${movieId}`, {
         headers:{ Authorization:`Bearer ${token}`},
     })
     .then(() =>{
@@ -70,5 +70,69 @@ removeFavoriteMovie(){
     const token = localStorage.getItem('item');
     const username = localStorage.getItem('user');
 
-    axios.put('https://edmund-movie-api.herokuapp.com/users/:Username')
+    axios.put(`https://edmund-movie-api.herokuapp.com/users/${username}`, {
+        headers: { Authorization: `Bearer ${token}` },
+        data: {
+          Name: newName ? newName : this.state.Name,
+          Username: newUsername ? newUsername : this.state.username,
+          Password: newPassword ? newPassword : this.state.password,
+          Email: newEmail ? newEmail : this.state.email,
+          Birthdate: newBirthday ? newBirthday : this.state.birthday,
+        },
+      })
+        .then((response) => {
+          alert('Saved Changes');
+          this.setState({
+            Name: response.data.Name,
+            Username: response.data.username,
+            Password: response.data.password,
+            Email: response.data.email,
+            Birthday: response.data.birthday,
+          });
+          localStorage.setItem('user', this.state.username);
+          window.open('/users/:username', '_self');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+ }
+    setName(input) {
+      this.Name = input;
+    }
+  
+    setUsername(input) {
+      this.username = input;
+    }
+  
+    setPassword(input) {
+      this.password = input;
+    }
+  
+    setEmail(input) {
+      this.email = input;
+    }
+  
+    setBirthday(input) {
+      this.birthday = input;
+    }
 }
+
+handleDeleteUser(e); {
+    e.preventDefault();
+
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
+
+    axios.delete(`https://edmund-movie-api.herokuapp.com/users/${username}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then(() => {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        alert('Your account has been deleted.');
+        window.open(`/`, '_self');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
